8be0c4f56c88c34895b8664179512ebbfcf2b7cd
Aecio Brito Ideapad <aeciobrito@gmail.com>
2021-12-08 11:13:38 -0300

    Dotnet 6 project with functional Startup class

---
+++ AspNet6_Startup/Startup.cs
@@ -0,0 +1,34 @@
+﻿namespace AspNet6_Startup
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(/*IApplicationBuiler*/ WebApplication app, IWebHostEnvironment environment)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
